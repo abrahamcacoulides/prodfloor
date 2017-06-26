1841dc954591338740dc2b6623f9bb68c19b639d
Abraham.Cacoulides <abraham.cacoulides@nidec-mce.com>
2017-06-26 15:45:57 -0500

    V0.403 Bug Fixed: *None
    Improvements:
    - Removed Unused Folders
    
    On Hold:
    IT: This app is still on hold
    
    Improvements proposals:
    *Separate the features for job type, e.g. MBRAKE should only be in 4000 etc

--- Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\abraham.cacoulides\PycharmProjects\testwebpage\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
